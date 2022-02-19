e2c312fb3dc4eef4278f2b25a1ad7098fdd48b28
WEEZOOKARP <76672483+WEEZOOKARP@users.noreply.github.com>
2022-02-20 12:32:59 +1300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,23 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+whitelistItems = whitelistItems or {}
+
+whitelistItems = {
+    `prop_traffic_01a`,
+    `prop_traffic_01b`,
+    `prop_traffic_01d`,
+    `prop_traffic_02a`,
+    `prop_traffic_02b`,
+    `prop_traffic_03a`,
+    `prop_traffic_lightset_01`,
+    `prop_streetlight_03`,
+    `prop_streetlight_03b`,
+    `prop_streetlight_03c`,
+    `prop_streetlight_03d`,
+    `prop_streetlight_04`,
+    `prop_streetlight_11c`
+}
+
 local item = "light_controller"
 
 local trafficLights = {
